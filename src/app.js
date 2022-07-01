03eba42da76e8fd901c123c9fa2d65d2ca0f527a
Eglany Junior <contas.eglanyjr@gmail.com>
2022-07-01 11:29:48 -0400

    :heavy_plus_sign: add dotenv and nodemon

--- src/app.js
+++ src/app.js
@@ -3,4 +3,4 @@ const express = require('express')
 const app = express();
 
 app.use(express.json())
-app.listen(3001, () => console.log('ouvindo porta 3000!'));
+app.listen(3001, () => console.log('ouvindo porta 3001!'));
