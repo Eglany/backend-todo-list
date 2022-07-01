1f4b67b7fa2e30624bc51b3010533ba9474a40b3
Eglany Junior <contas.eglanyjr@gmail.com>
2022-07-01 12:05:07 -0400

    :sparkles: MSC folders, connection file

--- src/app.js
+++ src/app.js
@@ -8,3 +8,7 @@ app.use(express.json())
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 app.get('/', (_request, response) => response.status(200).json({message: 'Backend TO-DO list '}))
+
+app.get('/task', (_request, response) => {
+
+})
