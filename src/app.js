911aecb446230440eb50285a575e99eed2b8b504
Eglany Junior <contas.eglanyjr@gmail.com>
2022-07-01 14:01:00 -0400

    :sparkles: implements functionality in tasks route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const taskController = require('./controller/taskController');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,5 @@ app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 app.get('/', (_request, response) => response.status(200).json({ message: 'Backend TO-DO list ' }));
 
-// app.get('/task', (_request, response) => {
-
-// })
+app.get('/task', taskController.getAll);
+app.post('/task', taskController.create);
